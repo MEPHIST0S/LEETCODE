8783d721944ac3e3f40af790042a47130b3f73f0
Murad Babayev <145931157+MEPHIST0S@users.noreply.github.com>
2024-03-05 03:28:43 +0400

    Commit

---
+++ 682 - Baseball Game.py
@@ -0,0 +1,18 @@
+class Solution:
+    def calPoints(self, operations):
+        ans = []
+        for i in operations:
+            if i == "+":
+                ans.append(ans[-1] + ans[-2])
+            elif i == "D":
+                ans.append(2 * ans[-1])
+            elif i == "C":
+                ans.pop()
+            else:
+                ans.append(int(i))
+        return sum(ans)
+
+#Example of Usage
+operations = ["5", "2", "C", "D", "+"]
+solution = Solution()
+print solution.calPoints(operations) 
