36face57b54d6dfa7c692fcfe4eec9601dfc0099
Murad Babayev <145931157+MEPHIST0S@users.noreply.github.com>
2024-03-05 03:37:38 +0400

    Commit

---
+++ 1929 - Concatenation.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def getConcatenation(self, nums):
+        return nums*2
+
+#Example of Usage    
+solution = Solution()
+nums = [1, 2, 3]
+result = solution.getConcatenation(nums)
+print(result)
