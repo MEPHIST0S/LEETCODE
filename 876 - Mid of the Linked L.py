4e5bdd8da41b5da82067cd5b0e483f8c0cdbc2a6
Murad Babayev <145931157+MEPHIST0S@users.noreply.github.com>
2024-03-05 22:59:02 +0400

    Commited

---
+++ 876 - Mid of the Linked L.py
@@ -0,0 +1,28 @@
+# Definition for singly-linked list.
+class ListNode(object):
+     def __init__(self, val=0, next=None):
+         self.val = val
+         self.next = next
+class Solution(object):
+    def middleNode(self, head):
+        slow = fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        return slow
+
+#Example of Usage!
+node1 = ListNode(1)
+node2 = ListNode(2)
+node3 = ListNode(3)
+node4 = ListNode(4)
+node5 = ListNode(5)
+
+node1.next = node2
+node2.next = node3
+node3.next = node4
+node4.next = node5
+
+sol = Solution()
+middle_node = sol.middleNode(node1)
+print(middle_node.val)
