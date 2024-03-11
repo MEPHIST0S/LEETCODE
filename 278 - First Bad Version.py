149549ff3e229a4a30413008b2f6e648db56ce2f
Murad Babayev <145931157+MEPHIST0S@users.noreply.github.com>
2024-03-12 01:48:24 +0400

    Commited

---
+++ 278 - First Bad Version.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def isBadVersion(self, version):
+        # Предположим, что здесь есть реализация функции isBadVersion()
+        pass
+
+    def firstBadVersion(self, n):
+        left, right = 1, n
+
+        while left < right:
+            mid = left + (right - left) // 2
+            if self.isBadVersion(mid):
+                right = mid
+            else:
+                left = mid + 1
+
+        return left
