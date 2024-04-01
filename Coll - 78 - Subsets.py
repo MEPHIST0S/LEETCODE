41007019b9fd62de504141f9955cf4c20aa6fdfd
Murad Babayev <145931157+MEPHIST0S@users.noreply.github.com>
2024-04-01 21:53:11 +0400

    Commited

---
+++ Coll - 78 - Subsets.py
@@ -0,0 +1,18 @@
+"""
+Given an integer array nums of unique elements, return all possible subsets (the power set).
+
+The solution set must not contain duplicate subsets. Return the solution in any order.
+"""
+
+class Solution(object):
+    def subset(self, nums):
+        arr = [[]]
+        for j in nums:
+            arr += [i + [j] for i in arr]
+        return arr
+
+#Example of Usage!
+solution = Solution()
+nums = [1, 2, 3]
+print(solution.subset(nums))
+        
